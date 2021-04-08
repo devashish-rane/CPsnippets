0e32bd15915d8562a83720724f48fd9ad020e443
Devashish Rane <ranedevashish2@gmail.com>
2021-04-08 23:09:21 +0530

    Fenwick Tree

---
+++ snippets/fenwick.cpp
@@ -0,0 +1,188 @@
+// Fenwick tree 
+// space linear
+// update log
+// query log
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+
+// //1 based indexing
+// struct Fenwick {
+//     vector<int> bit;  // binary indexed tree
+//     int n;
+
+//     Fenwick(int n) {
+//         this->n = n;
+//         bit.assign(n, 0);
+//     }
+
+//     Fenwick(vector<int> a) : Fenwick(a.size()) {
+//         for (int i = 0; i < a.size(); i++)
+//             add(i, a[i]);
+//     }
+
+//     int sum(int r) {
+//         int ret = 0;
+//         for (; r >= 0; r = (r & (r + 1)) - 1)
+//             ret += bit[r];
+//         return ret;
+//     }
+
+//     int sum(int l, int r) {
+//         return sum(r) - sum(l - 1);
+//     }
+
+//     void add(int idx, int delta) {
+//         for (; idx < n; idx = idx | (idx + 1))
+//             bit[idx] += delta;
+//     }
+// };
+
+
+
+// //min cumulative queries
+// struct FenwickTreeMin {
+//     vector<int> bit;
+//     int n;
+//     const int INF = (int)1e9;
+
+//     FenwickTreeMin(int n) {
+//         this->n = n;
+//         bit.assign(n, INF);
+//     }
+
+//     FenwickTreeMin(vector<int> a) : FenwickTreeMin(a.size()) {
+//         for (size_t i = 0; i < a.size(); i++)
+//             update(i, a[i]);
+//     }
+
+//     int getmin(int r) {
+//         int ret = INF;
+//         for (; r >= 0; r = (r & (r + 1)) - 1)
+//             ret = min(ret, bit[r]);
+//         return ret;
+//     }
+
+//     void update(int idx, int val) {
+//         for (; idx < n; idx = idx | (idx + 1))
+//             bit[idx] = min(bit[idx], val);
+//     }
+// };
+
+
+
+// // 2D 
+// struct FenwickTree2D {
+//     vector<vector<int>> bit;
+//     int n, m;
+
+//     // init(...) { ... }
+
+//     int sum(int x, int y) {
+//         int ret = 0;
+//         for (int i = x; i >= 0; i = (i & (i + 1)) - 1)
+//             for (int j = y; j >= 0; j = (j & (j + 1)) - 1)
+//                 ret += bit[i][j];
+//         return ret;
+//     }
+
+//     void add(int x, int y, int delta) {
+//         for (int i = x; i < n; i = i | (i + 1))
+//             for (int j = y; j < m; j = j | (j + 1))
+//                 bit[i][j] += delta;
+//     }
+// };
+
+
+
+// // 0 based indexing
+// struct FenwickTreeOneBasedIndexing {
+//     vector<int> bit;  // binary indexed tree
+//     int n;
+
+//     FenwickTreeOneBasedIndexing(int n) {
+//         this->n = n + 1;
+//         bit.assign(n + 1, 0);
+//     }
+
+//     FenwickTreeOneBasedIndexing(vector<int> a)
+//         : FenwickTreeOneBasedIndexing(a.size()) {
+//         for (size_t i = 0; i < a.size(); i++)
+//             add(i, a[i]);
+//     }
+
+//     int sum(int idx) {
+//         int ret = 0;
+//         for (++idx; idx > 0; idx -= idx & -idx)
+//             ret += bit[idx];
+//         return ret;
+//     }
+
+//     int sum(int l, int r) {
+//         return sum(r) - sum(l - 1);
+//     }
+
+//     void add(int idx, int delta) {
+//         for (++idx; idx < n; idx += idx & -idx)
+//             bit[idx] += delta;
+//     }
+// };
+
+
+
+// // range update point query
+// void add(int idx, int val) {
+//     for (++idx; idx < n; idx += idx & -idx)
+//         bit[idx] += val;
+// }
+
+// void range_add(int l, int r, int val) {
+//     add(l, val);
+//     add(r + 1, -val);
+// }
+
+// int point_query(int idx) {
+//     int ret = 0;
+//     for (++idx; idx > 0; idx -= idx & -idx)
+//         ret += bit[idx];
+//     return ret;
+// }
+
+
+// //Range Updates and Range Queries
+// def add(b, idx, x):
+//     while idx <= N:
+//         b[idx] += x
+//         idx += idx & -idx
+
+// def range_add(l,r,x):
+//     add(B1, l, x)    //B1 for range update
+//     add(B1, r+1, -x)
+//     add(B2, l, x*(l-1))   //B2 for range queries
+//     add(B2, r+1, -x*r)
+
+// def sum(b, idx): 
+//     total = 0
+//     while idx > 0:
+//         total += b[idx]
+//         idx -= idx & -idx
+//     return total
+
+// def prefix_sum(idx):
+//     return sum(B1, idx)*idx -  sum(B2, idx)
+
+// def range_sum(l, r):
+//     return prefix_sum(r) - prefix_sum(l-1)
+
+
+
+int main(){
+    vector<int> a={1,2,3,4,5,6,7,8,9};
+    FenwickTreeMin fwk = FenwickTreeMin(a);
+    cout<<fwk.getmin(1);
+
+    return 0;
+}
